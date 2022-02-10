86da81f1e1a94a96b875bf889d0a6d6473e6193a
sahbimeneer <antonlarens@gmail.com>
2022-02-10 17:06:08 +0100

    added comments

--- constants.py
+++ constants.py
@@ -1,9 +1,12 @@
 import os
 
+# Keys
 OPENSEA_API_KEY = os.environ.get("OPENSEA_API_KEY", "")
 DISCORD_TOKEN = os.environ.get("DISCORD_TOKEN", "")
 
+# Files
 NOTIFIERS_FILE_NAME = "notifiers.josn"
 BOT_SETTINGS_FILE_NAME = "bot.json"
 
+# Erros
 NOT_ENOUGH_ARGUMENTS = 0
